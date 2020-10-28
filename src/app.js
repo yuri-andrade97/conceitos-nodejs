4dd1810308f388292a9f4613917ef4ae66984363
Yuri Andrade <dev.yuri97@gmail.com>
2020-10-28 20:48:47 -0300

    Rota put configurada

--- src/app.js
+++ src/app.js
@@ -36,7 +36,25 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+  
+  const repository = {
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
